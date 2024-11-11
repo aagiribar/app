333e4d7cf1815a5336cfe8b195f6ac3b1162ece9
Agustín Alfonso González Iríbar <agustin.gonzalez112@alu.ulpgc.es>
2024-11-11 18:37:11 +0000

    Añadido mapa de España a la simulación

--- public/datos.js
+++ public/datos.js
@@ -1,3 +1,6 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
 // Latitud y longitud de los extremos del mapa de la imagen
 let minLon_es = -10.24;
 let maxLon_es = 5.03;
@@ -11,7 +14,16 @@ let maxLat_can = 29.473;
 
 let datosParo = []
 
-fetch("datos_paro.csv")
+let mapaEs, mapaCan
+
+let escena, camara, renderer;
+let controlOrbital;
+
+init();
+animationLoop();
+
+function init() {
+    fetch("datos_paro.csv")
     .then(respuesta => {
         if (!respuesta.ok) {
             throw new Error("Error: " + respuesta.statusText);
@@ -24,6 +36,27 @@ fetch("datos_paro.csv")
     .catch(error => {
         console.error("Error al cargar el archivo", error);
     });
+
+    escena = new THREE.Scene();
+
+    camara = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+    )
+
+    camara.position.z = 5;
+
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    controlOrbital = new OrbitControls(camara, renderer.domElement);
+
+    mapaEs = Plano(0, 0, 0);
+    texturizarPlano(mapaEs, "mapa_es.png")
+}
 
 
 function procesarDatosParo(contenido) {
@@ -52,4 +85,45 @@ function procesarDatosParo(contenido) {
     }
 
     console.log("Archivo con datos de paro cargado");
+}
+
+function Plano(x, y, z) {
+    let geometria = new THREE.PlaneBufferGeometry(5, 5);
+    let material = new THREE.MeshBasicMaterial({});
+    let mesh = new THREE.Mesh(geometria, material);
+    
+    mesh.position.set(x, y, z);
+    mesh.userData.mapsX = 5;
+    mesh.userData.mapsY = 5;
+    escena.add(mesh);
+    return mesh;
+}
+
+function texturizarPlano(plano, textura) {
+    new THREE.TextureLoader().load(
+        textura,
+        function(textura) {
+            plano.material.map = textura;
+            plano.material.needsUpdate = true;
+
+            const txHeight = textura.image.height;
+            const txWidth = textura.image.width;
+
+            if (txHeight > txWidth) {m
+                let factor = txHeight / txWidth;
+                plano.scale.set(1, factor, 1);
+                plano.userData.mapsY *= factor;
+            }
+            else {
+                let factor = txWidth / txHeight;
+                plano.scale.set(factor, 1, 1);
+                plano.userData.mapsX *= factor;
+            }
+        }
+    )
+}
+
+function animationLoop() {
+    requestAnimationFrame(animationLoop);
+    renderer.render(escena, camara);
 }
